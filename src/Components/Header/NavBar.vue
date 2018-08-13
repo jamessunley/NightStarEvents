8b2ba9ff934e53f3fe7ac2539ae4cdf33ba26e2e
James Sunley <jamessunley@jamess-mbp-2.home>
2018-08-13 21:30:04 +0100

    Split nav bar component and began adding header

---
+++ src/Components/Header/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <!-- <li class="nav-item"> -->
+            <router-link class="nav-link active" tag="li" active-class="active" to="/">Home</router-link>
+          <!-- </li> -->
+          <li class="nav-item">
+               <router-link class="nav-link" tag="li" active-class="active" to="/gallery">Gallery</router-link>
+          </li>
+          <li class="nav-item">
+             <router-link class="nav-link"  tag="li" active-class="active" to="/pricing">Pricing</router-link>
+          </li>
+          <li class="nav-item">
+               <router-link class="nav-link" tag="li" active-class="active"  to="/blog">Blog</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link class="nav-link" tag="li" active-class="active"  to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</template>
