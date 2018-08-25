30874795de2961e0e83cc0c075f264b5bb73c2ef
James Sunley <jamessunley@Jamess-MacBook-Pro-2.local>
2018-08-25 09:47:04 +0100

    added images on front page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
 
 import { routes } from './routes';
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
    routes: routes,
